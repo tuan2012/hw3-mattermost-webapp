999b61a9ce927cd83369d4892ab717058e4a397c
ngthotuan <ngthotuan@gmail.com>
2021-06-08 16:23:05 +0000

    add test

---
+++ test-auto/Test Suites/Run All.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Run All</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7a4b773f-ba61-4680-a61e-1f17fd4dd603</testSuiteGuid>
+   <testCaseLink>
+      <guid>137842f0-263e-4013-9937-e91ae7ac72c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
