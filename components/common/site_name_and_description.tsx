2bb808d25eccfa359b457b1049e74bfd78578ab5
Minhvu17ctt7 <Minhvu1712918@gmail.com>
2021-06-12 22:25:58 +0700

    update desciption

--- components/common/site_name_and_description.tsx
+++ components/common/site_name_and_description.tsx
@@ -40,7 +40,7 @@ export default class SiteNameAndDescription extends React.PureComponent<Props> {
                 >
                     {description}
                 </h4>
-                <h4>HW3 - Xin chào thầy</h4>
+                <h4>HW3 - NHÓM 01 XIN CHAO THAY</h4>
             </React.Fragment>
         );
     }
