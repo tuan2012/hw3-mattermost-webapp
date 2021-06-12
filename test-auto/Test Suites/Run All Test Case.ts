c9228365c1ed4f1d6fed97da0177157db7ee56eb
Minhvu17ctt7 <Minhvu1712918@gmail.com>
2021-06-12 21:07:44 +0700

    update

---
+++ test-auto/Test Suites/Run All Test Case.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Run All Test Case</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>15</pageLoadTimeout>
+   <pageLoadTimeoutDefault>false</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7a4b773f-ba61-4680-a61e-1f17fd4dd603</testSuiteGuid>
+   <testCaseLink>
+      <guid>137842f0-263e-4013-9937-e91ae7ac72c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2ee05574-8cf2-489c-9b3e-66306b8a0f8c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
